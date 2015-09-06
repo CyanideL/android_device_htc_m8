81344a17b5b3e0893273d542f1a4a8b516a59ad7
rogersb11 <brettrogers11@gmail.com>
2015-09-06 03:18:52 -0400

    Cyanide

--- cyanide.mk
+++ cyanide.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cyanide/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common CYANIDE stuff.
+$(call inherit-product, vendor/cyanide/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_m8
+PRODUCT_NAME := cyanide_m8
